47af8e199fbfd31fdca42e1e4a9c3f2376591379
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-27 17:01:48 -0500

    first commit

---
+++ test_jd.py
@@ -0,0 +1,13 @@
+from can_jd import *
+from axotec.gps import *
+import numpy as np
+
+#CAN
+can_jd = CANJD(port, id)    
+
+
+while True:
+    speed = can_jd.get_speed_stimation()
+    print(f"velocidad: {speed} m/s")
+
+
